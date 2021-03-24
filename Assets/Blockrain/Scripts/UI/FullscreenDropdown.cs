794a163fcdc6e3fe3d94e006ce9ff65bf1da2d91
James Mills <james.mills39@tafensw.edu.au>
2021-03-24 16:29:37 +1100

    Initial Commit

---
+++ Assets/Blockrain/Scripts/UI/FullscreenDropdown.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+using TMPro;
+
+using Enum = System.Enum;
+using System.Collections.Generic;
+
+namespace Blockrain.UI
+{
+    public class FullscreenDropdown : OptionsDropdown
+    {
+        protected override int GetDefaultValue() => (int)FullScreenMode.FullScreenWindow;
+
+        protected override List<TMP_Dropdown.OptionData> GetOptions()
+        {
+            List<TMP_Dropdown.OptionData> options = new List<TMP_Dropdown.OptionData>();
+
+            foreach(FullScreenMode mode in Enum.GetValues(typeof(FullScreenMode)))
+                options.Add(new TMP_Dropdown.OptionData(mode.ToString()));
+
+            return options;
+        }
+
+        protected override void OnOptionSelected(int _option) => Screen.fullScreenMode = (FullScreenMode)_option;
+    }
+}
---
+++ Assets/Blockrain/Scripts/UI/ResolutionDropdown.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+using TMPro;
+
+using System.Collections.Generic;
+
+namespace Blockrain.UI
+{ 
+    public class ResolutionDropdown : OptionsDropdown
+    {
+        private List<Resolution> resolutions;
+
+        protected override void Start()
+        {
+            resolutions = new List<Resolution>(Screen.resolutions);
+
+            base.Start();
+        }
+
+        protected override int GetDefaultValue() => resolutions.IndexOf(Screen.currentResolution);
+
+        protected override List<TMP_Dropdown.OptionData> GetOptions()
+        {
+            List<TMP_Dropdown.OptionData> options = new List<TMP_Dropdown.OptionData>();
+
+            foreach (Resolution res in resolutions)
+                options.Add(new TMP_Dropdown.OptionData($"{res.width}x{res.height}"));
+
+            return options;
+        }
+
+        protected override void OnOptionSelected(int _option)
+        {
+            Resolution res = resolutions[_option];
+
+            Screen.SetResolution(res.width, res.height, Screen.fullScreenMode, res.refreshRate);
+        }
+    }
+}
