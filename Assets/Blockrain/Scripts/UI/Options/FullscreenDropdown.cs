a2d97d57a66c7d89e92e6d4c27de891a14a0132f
JamesMillsTAFE <james.mills39@tafensw.edu.au>
2021-05-03 16:46:01 +1000

    Movement

---
+++ Assets/Blockrain/Scripts/UI/Options/FullscreenDropdown.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+using TMPro;
+
+using Enum = System.Enum;
+using System.Collections.Generic;
+
+namespace Blockrain.UI.Options
+{
+    public class FullscreenDropdown : OptionsDropdown
+    {
+        protected override string Option => "Fullscreen";
+
+        protected override int GetDefaultValue() => (int)FullScreenMode.FullScreenWindow;
+
+        protected override List<TMP_Dropdown.OptionData> GetOptions()
+        {
+            List<TMP_Dropdown.OptionData> options = new List<TMP_Dropdown.OptionData>();
+
+            foreach(FullScreenMode mode in Enum.GetValues(typeof(FullScreenMode)))
+                options.Add(new TMP_Dropdown.OptionData(mode.ToString()));
+
+            return options;
+        }
+
+        protected override void OnOptionSelected(int _option) => Screen.fullScreenMode = (FullScreenMode)_option;
+    }
+}
---
+++ Assets/Blockrain/Scripts/UI/Screens/OptionsScreen.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Blockrain.UI.Screens
+{
+    public class OptionsScreen : MenuScreen
+    {
+        protected override void Run()
+        {
+            
+        }
+    }
+}
